a9ff252f7268a77d2ab52720ceec0d39479af8a3
Christian Salazar <salazar3148@gmail.com>
2023-05-24 23:41:14 -0500

    hu03

---
+++ src/main/java/com/pragma/powerup/plazamicroservice/adapters/driving/http/dto/request/DishRequestDto.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup.plazamicroservice.adapters.driving.http.dto.request;
+
+public class DishRequestDto {
+    private String nombre;
+    private int precio;
+    private String descripcion;
+    private String urlImagen;
+    private String categoria;
+    private Long nitPlaza;
+    private boolean activo;
+}
